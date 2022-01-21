fd4e4663eadc0ac914ee243065d6787d6d029182
fahrulseptiana <fahrulseptiana@users.noreply.github.com>
2022-01-21 08:41:03 +0000

    build: transform tokens

--- output/js/dark.js
+++ output/js/dark.js
@@ -1,13 +1,13 @@
 /**
  * Do not edit directly
- * Generated on Fri, 21 Jan 2022 08:32:24 GMT
+ * Generated on Fri, 21 Jan 2022 08:41:02 GMT
  */
 
-export const default = "#ffffff";
-export const muted = "#e2e8f0";
-export const subtle = "#a0aec0";
-export const default = "#1a202c";
-export const muted = "#4a5568";
-export const subtle = "#718096";
-export const default = "#5a67d8";
-export const default = "#f7fafc";
+export const fgDefault = "#ffffff";
+export const fgMuted = "#e2e8f0";
+export const fgSubtle = "#a0aec0";
+export const bgDefault = "#1a202c";
+export const bgMuted = "#4a5568";
+export const bgSubtle = "#718096";
+export const accentDefault = "#5a67d8";
+export const shadowsDefault = "#f7fafc";
--- output/js/theme-light.js
+++ output/js/theme-light.js
@@ -1,19 +1,19 @@
 /**
  * Do not edit directly
- * Generated on Fri, 21 Jan 2022 08:32:24 GMT
+ * Generated on Fri, 21 Jan 2022 08:41:02 GMT
  */
 
-export const ghost = "4px";
-export const ghost = "8px";
-export const card = "4px";
-export const title = "13.6px";
-export const content = "10.4px";
-export const backgroundColor = "#FFFF00";
-export const default = "#000000";
-export const muted = "#4a5568";
-export const subtle = "#a0aec0";
-export const default = "#ffffff";
-export const muted = "#f7fafc";
-export const subtle = "#edf2f7";
-export const default = "#7f9cf5";
-export const default = "#1a202c";
+export const borderWidthButtonGhost = "4px";
+export const borderRadiusButtonGhost = "8px";
+export const borderRadiusCard = "4px";
+export const fontSizesCardTitle = "13.6px";
+export const fontSizesCardContent = "10.4px";
+export const buttonsSolidBackgroundColor = "#FFFF00";
+export const fgDefault = "#000000";
+export const fgMuted = "#4a5568";
+export const fgSubtle = "#a0aec0";
+export const bgDefault = "#ffffff";
+export const bgMuted = "#f7fafc";
+export const bgSubtle = "#edf2f7";
+export const accentDefault = "#7f9cf5";
+export const shadowsDefault = "#1a202c";
