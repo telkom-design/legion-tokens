75ca5ef6a6b950baa2afa78e5c4dec8eb3f01a77
fahrulseptiana <fahrulseptiana@users.noreply.github.com>
2022-01-21 06:11:10 +0000

    build: transform tokens

---
+++ output/js/light.js
@@ -0,0 +1,167 @@
+/**
+ * Do not edit directly
+ * Generated on Fri, 21 Jan 2022 06:11:10 GMT
+ */
+
+var _styleDictionary = {
+  "fg": {
+    "default": {
+      "value": "#000000",
+      "type": "color",
+      "filePath": "tokens/light.json",
+      "isSource": true,
+      "original": {
+        "value": "#000000",
+        "type": "color"
+      },
+      "name": "fg-default",
+      "attributes": {
+        "category": "fg",
+        "type": "default"
+      },
+      "path": [
+        "fg",
+        "default"
+      ]
+    },
+    "muted": {
+      "value": "#4a5568",
+      "type": "color",
+      "filePath": "tokens/light.json",
+      "isSource": true,
+      "original": {
+        "value": "#4a5568",
+        "type": "color"
+      },
+      "name": "fg-muted",
+      "attributes": {
+        "category": "fg",
+        "type": "muted"
+      },
+      "path": [
+        "fg",
+        "muted"
+      ]
+    },
+    "subtle": {
+      "value": "#a0aec0",
+      "type": "color",
+      "filePath": "tokens/light.json",
+      "isSource": true,
+      "original": {
+        "value": "#a0aec0",
+        "type": "color"
+      },
+      "name": "fg-subtle",
+      "attributes": {
+        "category": "fg",
+        "type": "subtle"
+      },
+      "path": [
+        "fg",
+        "subtle"
+      ]
+    }
+  },
+  "bg": {
+    "default": {
+      "value": "#ffffff",
+      "type": "color",
+      "filePath": "tokens/light.json",
+      "isSource": true,
+      "original": {
+        "value": "#ffffff",
+        "type": "color"
+      },
+      "name": "bg-default",
+      "attributes": {
+        "category": "bg",
+        "type": "default"
+      },
+      "path": [
+        "bg",
+        "default"
+      ]
+    },
+    "muted": {
+      "value": "#f7fafc",
+      "type": "color",
+      "filePath": "tokens/light.json",
+      "isSource": true,
+      "original": {
+        "value": "#f7fafc",
+        "type": "color"
+      },
+      "name": "bg-muted",
+      "attributes": {
+        "category": "bg",
+        "type": "muted"
+      },
+      "path": [
+        "bg",
+        "muted"
+      ]
+    },
+    "subtle": {
+      "value": "#edf2f7",
+      "type": "color",
+      "filePath": "tokens/light.json",
+      "isSource": true,
+      "original": {
+        "value": "#edf2f7",
+        "type": "color"
+      },
+      "name": "bg-subtle",
+      "attributes": {
+        "category": "bg",
+        "type": "subtle"
+      },
+      "path": [
+        "bg",
+        "subtle"
+      ]
+    }
+  },
+  "accent": {
+    "default": {
+      "value": "#7f9cf5",
+      "type": "color",
+      "filePath": "tokens/light.json",
+      "isSource": true,
+      "original": {
+        "value": "#7f9cf5",
+        "type": "color"
+      },
+      "name": "accent-default",
+      "attributes": {
+        "category": "accent",
+        "type": "default"
+      },
+      "path": [
+        "accent",
+        "default"
+      ]
+    }
+  },
+  "shadows": {
+    "default": {
+      "value": "#1a202c",
+      "type": "color",
+      "filePath": "tokens/light.json",
+      "isSource": true,
+      "original": {
+        "value": "#1a202c",
+        "type": "color"
+      },
+      "name": "shadows-default",
+      "attributes": {
+        "category": "shadows",
+        "type": "default"
+      },
+      "path": [
+        "shadows",
+        "default"
+      ]
+    }
+  }
+};
---
+++ output/js/theme-light.js
@@ -0,0 +1,351 @@
+/**
+ * Do not edit directly
+ * Generated on Fri, 21 Jan 2022 06:11:10 GMT
+ */
+
+var _styleDictionary = {
+  "button": {
+    "primary": {
+      "background": {
+        "value": "#7f9cf5",
+        "type": "color",
+        "filePath": "tokens/theme-light.json",
+        "isSource": true,
+        "original": {
+          "value": "#7f9cf5",
+          "type": "color"
+        },
+        "name": "button-primary-background",
+        "attributes": {
+          "category": "button",
+          "type": "primary",
+          "item": "background"
+        },
+        "path": [
+          "button",
+          "primary",
+          "background"
+        ]
+      }
+    },
+    "ghost": {
+      "outline": {
+        "value": "#000000",
+        "type": "color",
+        "filePath": "tokens/theme-light.json",
+        "isSource": true,
+        "original": {
+          "value": "#000000",
+          "type": "color"
+        },
+        "name": "button-ghost-outline",
+        "attributes": {
+          "category": "button",
+          "type": "ghost",
+          "item": "outline"
+        },
+        "path": [
+          "button",
+          "ghost",
+          "outline"
+        ]
+      },
+      "text": {
+        "value": "#000000",
+        "type": "color",
+        "filePath": "tokens/theme-light.json",
+        "isSource": true,
+        "original": {
+          "value": "#000000",
+          "type": "color"
+        },
+        "name": "button-ghost-text",
+        "attributes": {
+          "category": "button",
+          "type": "ghost",
+          "item": "text"
+        },
+        "path": [
+          "button",
+          "ghost",
+          "text"
+        ]
+      }
+    }
+  },
+  "borderWidth": {
+    "button": {
+      "ghost": {
+        "value": "4px",
+        "type": "borderWidth",
+        "filePath": "tokens/theme-light.json",
+        "isSource": true,
+        "original": {
+          "value": 4,
+          "type": "borderWidth"
+        },
+        "name": "border-width-button-ghost",
+        "attributes": {
+          "category": "borderWidth",
+          "type": "button",
+          "item": "ghost"
+        },
+        "path": [
+          "borderWidth",
+          "button",
+          "ghost"
+        ]
+      }
+    }
+  },
+  "borderRadius": {
+    "button": {
+      "ghost": {
+        "value": "8px",
+        "type": "borderRadius",
+        "filePath": "tokens/theme-light.json",
+        "isSource": true,
+        "original": {
+          "value": 8,
+          "type": "borderRadius"
+        },
+        "name": "border-radius-button-ghost",
+        "attributes": {
+          "category": "borderRadius",
+          "type": "button",
+          "item": "ghost"
+        },
+        "path": [
+          "borderRadius",
+          "button",
+          "ghost"
+        ]
+      }
+    },
+    "card": {
+      "value": "4px",
+      "type": "borderRadius",
+      "filePath": "tokens/theme-light.json",
+      "isSource": true,
+      "original": {
+        "value": 4,
+        "type": "borderRadius"
+      },
+      "name": "border-radius-card",
+      "attributes": {
+        "category": "borderRadius",
+        "type": "card"
+      },
+      "path": [
+        "borderRadius",
+        "card"
+      ]
+    }
+  },
+  "fontSizes": {
+    "card": {
+      "title": {
+        "value": "13.6px",
+        "type": "fontSizes",
+        "filePath": "tokens/theme-light.json",
+        "isSource": true,
+        "original": {
+          "value": 13.6,
+          "type": "fontSizes"
+        },
+        "name": "font-sizes-card-title",
+        "attributes": {
+          "category": "fontSizes",
+          "type": "card",
+          "item": "title"
+        },
+        "path": [
+          "fontSizes",
+          "card",
+          "title"
+        ]
+      },
+      "content": {
+        "value": "10.4px",
+        "type": "fontSizes",
+        "filePath": "tokens/theme-light.json",
+        "isSource": true,
+        "original": {
+          "value": 10.4,
+          "type": "fontSizes"
+        },
+        "name": "font-sizes-card-content",
+        "attributes": {
+          "category": "fontSizes",
+          "type": "card",
+          "item": "content"
+        },
+        "path": [
+          "fontSizes",
+          "card",
+          "content"
+        ]
+      }
+    }
+  },
+  "fg": {
+    "default": {
+      "value": "#000000",
+      "type": "color",
+      "filePath": "tokens/theme-light.json",
+      "isSource": true,
+      "original": {
+        "value": "#000000",
+        "type": "color"
+      },
+      "name": "fg-default",
+      "attributes": {
+        "category": "fg",
+        "type": "default"
+      },
+      "path": [
+        "fg",
+        "default"
+      ]
+    },
+    "muted": {
+      "value": "#4a5568",
+      "type": "color",
+      "filePath": "tokens/theme-light.json",
+      "isSource": true,
+      "original": {
+        "value": "#4a5568",
+        "type": "color"
+      },
+      "name": "fg-muted",
+      "attributes": {
+        "category": "fg",
+        "type": "muted"
+      },
+      "path": [
+        "fg",
+        "muted"
+      ]
+    },
+    "subtle": {
+      "value": "#a0aec0",
+      "type": "color",
+      "filePath": "tokens/theme-light.json",
+      "isSource": true,
+      "original": {
+        "value": "#a0aec0",
+        "type": "color"
+      },
+      "name": "fg-subtle",
+      "attributes": {
+        "category": "fg",
+        "type": "subtle"
+      },
+      "path": [
+        "fg",
+        "subtle"
+      ]
+    }
+  },
+  "bg": {
+    "default": {
+      "value": "#ffffff",
+      "type": "color",
+      "filePath": "tokens/theme-light.json",
+      "isSource": true,
+      "original": {
+        "value": "#ffffff",
+        "type": "color"
+      },
+      "name": "bg-default",
+      "attributes": {
+        "category": "bg",
+        "type": "default"
+      },
+      "path": [
+        "bg",
+        "default"
+      ]
+    },
+    "muted": {
+      "value": "#f7fafc",
+      "type": "color",
+      "filePath": "tokens/theme-light.json",
+      "isSource": true,
+      "original": {
+        "value": "#f7fafc",
+        "type": "color"
+      },
+      "name": "bg-muted",
+      "attributes": {
+        "category": "bg",
+        "type": "muted"
+      },
+      "path": [
+        "bg",
+        "muted"
+      ]
+    },
+    "subtle": {
+      "value": "#edf2f7",
+      "type": "color",
+      "filePath": "tokens/theme-light.json",
+      "isSource": true,
+      "original": {
+        "value": "#edf2f7",
+        "type": "color"
+      },
+      "name": "bg-subtle",
+      "attributes": {
+        "category": "bg",
+        "type": "subtle"
+      },
+      "path": [
+        "bg",
+        "subtle"
+      ]
+    }
+  },
+  "accent": {
+    "default": {
+      "value": "#7f9cf5",
+      "type": "color",
+      "filePath": "tokens/theme-light.json",
+      "isSource": true,
+      "original": {
+        "value": "#7f9cf5",
+        "type": "color"
+      },
+      "name": "accent-default",
+      "attributes": {
+        "category": "accent",
+        "type": "default"
+      },
+      "path": [
+        "accent",
+        "default"
+      ]
+    }
+  },
+  "shadows": {
+    "default": {
+      "value": "#1a202c",
+      "type": "color",
+      "filePath": "tokens/theme-light.json",
+      "isSource": true,
+      "original": {
+        "value": "#1a202c",
+        "type": "color"
+      },
+      "name": "shadows-default",
+      "attributes": {
+        "category": "shadows",
+        "type": "default"
+      },
+      "path": [
+        "shadows",
+        "default"
+      ]
+    }
+  }
+};
