2743d96f2acbd66310344714130101517f8d7115
fahrulseptiana <fahrulseptiana@users.noreply.github.com>
2022-01-24 18:46:42 +0000

    build: transform tokens

---
+++ output/js/logee-sales.js
@@ -0,0 +1,29 @@
+/**
+ * Do not edit directly
+ * Generated on Mon, 24 Jan 2022 18:46:42 GMT
+ */
+
+export const buttonsLgPaddingLeft = 16;
+export const buttonsLgPaddingRight = 16;
+export const buttonsLgPaddingTop = 8;
+export const buttonsLgPaddingBottom = 8;
+export const buttonsLgFontSize = 16;
+export const buttonsSmPaddingLeft = 8;
+export const buttonsSmPaddingRight = 8;
+export const buttonsSmPaddingTop = 4;
+export const buttonsSmPaddingBottom = 4;
+export const buttonsSmFontSize = 13.6;
+export const buttonsSolidFontFamily = "Roboto";
+export const buttonsSolidBackgroundColor = "#4299e1";
+export const buttonsSolidHoverBackgroundColor = "#3182ce";
+export const buttonsSolidActiveBackgroundColor = "#2b6cb0";
+export const buttonsSolidFocusBackgroundColor = "#4299e1";
+export const buttonsSolidDisabledBackgroundColor = "#e2e8f0";
+export const buttonsSolidDisabledColor = "#000";
+export const buttonsSoftFontFamily = "Roboto";
+export const buttonsSoftBackgroundColor = "#4299e1";
+export const buttonsSoftHoverBackgroundColor = "#3182ce";
+export const buttonsSoftActiveBackgroundColor = "#2b6cb0";
+export const buttonsSoftFocusBackgroundColor = "#4299e1";
+export const buttonsSoftDisabledBackgroundColor = "#e2e8f0";
+export const buttonsSoftDisabledColor = "#000";
